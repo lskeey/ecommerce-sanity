c07503395f162a710350f5baab6d4782d1ef1ef7
Muhammad Alviansyah <muhammd.alviansyah@gmail.com>
2024-11-06 20:18:39 +0700

    chore(sanity): add typegen for product, category, order, and sale types

---
+++ sanity/schemaTypes/saleType.ts
@@ -0,0 +1,59 @@
+import {TagIcon} from "@sanity/icons"
+import {defineField, defineType} from "sanity"
+
+export const saleType = defineType({
+  name: "sales",
+  title: "Sales",
+  type: "document",
+  icon: TagIcon,
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+      title: "Sale Title",
+    }),
+    defineField({
+      name: "description",
+      type: "text",
+      title: "Sale Description",
+    }),
+    defineField({
+      name: "couponCode",
+      type: "string",
+      title: "Coupon Code",
+    }),
+    defineField({
+      name: "validForm",
+      type: "datetime",
+      title: "Valid Form",
+    }),
+    defineField({
+      name: "validUntil",
+      type: "datetime",
+      title: "Valid Until",
+    }),
+    defineField({
+      name: "isActive",
+      type: "boolean",
+      title: "Is Active",
+      description: "Toogle to activate/deactivate the sale",
+      initialValue: true,
+    }),
+  ],
+  preview: {
+    select: {
+      title: "title",
+      discountAmount: "discountAmount",
+      couponCode: "couponCode",
+      isActive: "isActive",
+    },
+    prepare(selection) {
+      const { title, discountAmount, couponCode, isActive } = selection;
+      const status = isActive ? "Active" : "Inactive";
+      return {
+        title,
+        subtitle: `${discountAmount}% off - code: ${couponCode} - ${status}`
+      };
+    },
+  },
+});
