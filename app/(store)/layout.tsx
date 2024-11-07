6810311f0e7981ffb0b10bef216c8b6707ff318c
Muhammad Alviansyah <muhammd.alviansyah@gmail.com>
2024-11-07 09:13:07 +0700

    feat(helpers): add function to fetch all products with GROQ query

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <Header />
-          <main>
+          <main className="container mx-auto max-w-5xl">
             {children}
           </main>
         </body>
--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
+
+export default async function Home() {
+  const products = await getAllProducts();
+  console.log(products)
   return (
     <div>Hello World</div>
   );
