6a1882755b8b88b39e6b481280ea9f5813420b96
Muhammad Alviansyah <muhammd.alviansyah@gmail.com>
2024-11-13 20:19:15 +0700

    feat(search): building the search page

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,3 +1,6 @@
+import ProductGrid from "@/components/products/product-grid";
+import { searchProductByName } from "@/sanity/lib/products/searchProductsByName";
+
 export default async function SearchPage({
   searchParams
 }: {
@@ -7,7 +10,21 @@ export default async function SearchPage({
 }) {
   const { query } = await searchParams;
 
+  const products = await searchProductByName(query);
+
+  if (!products.length) {
+    return (
+      <div className="text-center py-6 space-y-3 border shadow-md rounded-md">
+        <h1 className="text-xl font-bold">No products found for: {query}</h1>
+        <p className="text-sm text-gray-500">Try searching with different keywords</p>
+      </div>
+    )
+  }
+
   return (
-    <div>SearchPage for {query}</div>
+    <div>
+      <h1>Search results for {query}</h1>
+      <ProductGrid products={products} />
+    </div>
   )
 }
