ebe39e51b33c566003bf78fdbe07c55766539bc3
Muhammad Alviansyah <muhammd.alviansyah@gmail.com>
2024-11-14 11:22:21 +0700

    style(home): add padding to home page layout

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -5,10 +5,10 @@ import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 export default async function Home() {
   const products = await getAllProducts();
   return (
-    <>
+    <div className="px-1 md:px-4">
       <BlackFridayBanner />
       <ProductGrid products={products} />
-    </>
+    </div>
   );
 }
   
