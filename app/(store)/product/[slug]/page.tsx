8b59f71e75af2328afbab99c808bcfc3402737b3
Muhammad Alviansyah <muhammd.alviansyah@gmail.com>
2024-11-21 10:34:27 +0700

    fix: standardize height of product cards for consistent layout

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import AddToBasketCard from "@/components/products/add-to-basket-card";
+import ProductInfo from "@/components/products/product-info";
 import { imageUrl } from "@/sanity/lib/imageUrl";
 import { getProductBySlug } from "@/sanity/lib/products/getProductBySlug";
 import Image from "next/image";
@@ -7,7 +9,6 @@ import { notFound } from "next/navigation";
 const ProductPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params;
   const product = await getProductBySlug(slug);
-  console.log(product);
 
   if (!product) {
     return notFound();
@@ -16,33 +17,32 @@ const ProductPage = async ({ params }: { params: Promise<{ slug: string }> }) =>
   const isOutOfStock = product.stock != null && product.stock <= 0;
 
   return (
-    
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-1 md:px-4">
-      <div className="relative aspect-square w-full h-full">
-          {product.image ? (
-            <Image
-              src={imageUrl(product.image).url()}
-              alt={`Image of ${product.name}`}
-              fill
-              style={{
-                objectFit: "contain"
-              }}
-            />
-          ) : (
-            <div className="flex items-center justify-center w-full h-full bg-gray-200">
-              <span>No Image Available</span>
-            </div>
-          )}
-        </div>
+    <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 px-1 md:px-4">
+      <div className="relative lg:sticky top-4 aspect-square w-full border mx-auto">
+        {product.image ? (
+          <Image
+            src={imageUrl(product.image).url()}
+            alt={`Image of ${product.name}`}
+            fill
+            style={{
+              objectFit: "contain"
+            }}
+          />
+        ) : (
+          <div className="flex items-center justify-center w-full h-full bg-gray-200">
+            <span>No Image Available</span>
+          </div>
+        )}
         {isOutOfStock && (
           <div className="absolute top-0 left-0 bottom-0 right-0 flex justify-center items-center bg-black bg-opacity-30">
             <span className="text-white">Out of Stock</span>
           </div>
         )}
-        <div className="space-y-2">
-          <div className="text-md font-bold">{product.name}</div>
-          <div className="text-2xl font-bold">${product.price}</div>
-        </div>
+      </div>
+      <div className="col-span-2">
+        <ProductInfo product={product} />
+      </div>
+      <AddToBasketCard product={product} disabled={isOutOfStock} />
     </div>
   )
 }
