0c0b2ea7560d6a8e1676ae7ebf6a5e9fcceb1999
Muhammad Alviansyah <muhammd.alviansyah@gmail.com>
2024-11-21 10:23:06 +0700

    feat: display cart item count on "My Cart" button in header

--- components/header.tsx
+++ components/header.tsx
@@ -10,9 +10,15 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 import { ClerkLoaded, SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import useBasketStore from "@/store/store";
 
 export default function Header() {
   const { user } = useUser();
+
+  const itemCount = useBasketStore((state) =>
+    state.items.reduce((total, item) => total + item.quantity, 0)
+  );
+
   return (
     <header className="flex justify-between items-center px-1 md:px-4 py-4 gap-4">
       <Link href="/" className="text-xl md:text-2xl font-extrabold uppercase">shop</Link>
@@ -25,11 +31,17 @@ export default function Header() {
       </Form>
 
       <div className="flex gap-2 items-center">
-        <Button size="sm" variant={"outline"} className="rounded-sm">
+        <Button size="sm" variant={"outline"} className="relative rounded-sm">
           <Link href="/cart" className="flex gap-1">
             <IoCartOutline className="w-6 h-6" />
             <span className="hidden md:block">My Cart</span>
           </Link>
+
+          {itemCount > 0 && (
+            <div className="w-4 h-4 absolute -top-2 -right-1 bg-red-500 text-white flex items-center justify-center rounded-full text-[9px]">
+              {itemCount}
+            </div>
+          )}
         </Button>
 
         <ClerkLoaded>
